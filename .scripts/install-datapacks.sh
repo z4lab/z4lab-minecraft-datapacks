8c7c113dc0e4bd44491aa3d9c60dbe8e73b8ac50
Riccardo H <ace_graphics@outlook.de>
2019-07-31 13:50:21 +0200

    shortening script

--- .scripts/install-datapacks.sh
+++ .scripts/install-datapacks.sh
@@ -1,2 +1,1 @@
-rm -rf datapacks
-git clone https://github.com/13ace37/z4lab-minecraft-datapacks.git datapacks
+rm -rf datapacks && git clone https://github.com/13ace37/z4lab-minecraft-datapacks.git datapacks
