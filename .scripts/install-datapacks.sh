3f760d1bc2ea59f771ebbb90218ec9ffd79ac2b1
Riccardo H <Ace_Graphics@outlook.de>
2020-01-10 19:14:53 +0100

    Update install-datapacks.sh

--- .scripts/install-datapacks.sh
+++ .scripts/install-datapacks.sh
@@ -1,1 +1,1 @@
-rm -rf datapacks && git clone https://github.com/13ace37/z4lab-minecraft-datapacks.git datapacks
+rm -rf datapacks && git clone https://github.com/z4lab/z4lab-minecraft-datapacks.git datapacks
